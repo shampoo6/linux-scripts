f2f38cb8522faa7a4958a3b34ec6fae7c8011425
shampoo6 <shampoo6@163.com>
2020-11-11 08:50:12 +0800

    注释docker安装时候设置镜像地址的代码

--- docker-install-centos7.sh
+++ docker-install-centos7.sh
@@ -26,18 +26,18 @@ fi
 
 # 设置镜像地址
 # 发现设置镜像地址后，速度反而变慢了
-echo 'start set docker mirrors'
-echo '{
-  "registry-mirrors": [
-    "https://xznw8bfn.mirror.aliyuncs.com",
-    "https://hub-mirror.c.163.com",
-    "https://mirror.baidubce.com"
-  ]
-}' > /etc/docker/daemon.json
-chmod -R 777 /etc/docker/daemon.json
-systemctl daemon-reload
-systemctl restart docker
-echo 'set docker mirrors over'
+# echo 'start set docker mirrors'
+# echo '{
+#   "registry-mirrors": [
+#     "https://xznw8bfn.mirror.aliyuncs.com",
+#     "https://hub-mirror.c.163.com",
+#     "https://mirror.baidubce.com"
+#   ]
+# }' > /etc/docker/daemon.json
+# chmod -R 777 /etc/docker/daemon.json
+# systemctl daemon-reload
+# systemctl restart docker
+# echo 'set docker mirrors over'
 
 # 运行 demo 如果执行成功说明安装完成
 docker run hello-world
